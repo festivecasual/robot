fdadf7496c2a4c021f8c4206398c0016071daa76
Brandon McCalip <bmccalip@gmail.com>
2018-09-21 20:23:02 -0500

    Calibrated installed servos, revised test pattern

--- servo.py
+++ servo.py
@@ -11,26 +11,30 @@ pca = PCA9685(busio.I2C(SCL, SDA))
 pca.frequency = 50
 
 servo0 = servo.Servo(pca.channels[0], min_pulse=580, max_pulse=2480)
+servo1 = servo.Servo(pca.channels[1], min_pulse=750, max_pulse=2350)
+
+servo0.angle = 90
+servo1.angle = 90
+
+time.sleep(1)
+
+for i in range(90, 180):
+    servo0.angle = i
+    servo1.angle = i
+    time.sleep(0.01)
+
+for i in range(180, 0, -1):
+    servo0.angle = i
+    servo1.angle = i
+    time.sleep(0.01)
 
 servo0.angle = 0
+servo1.angle = 180
 
-for i in range(180):
-    time.sleep(0.01)
-    servo0.angle = i
+time.sleep(1)
 
-for i in range(180):
-    servo0.angle = 180 - i
-
-time.sleep(0.5)
-
-for i in range(90):
-    time.sleep(0.1)
-    servo0.angle = i
-
-time.sleep(0.5)
-
-for i in range(90):
-    servo0.angle = 90 - i
+servo0.angle = 180
+servo1.angle = 0
 
 pca.deinit()
 
